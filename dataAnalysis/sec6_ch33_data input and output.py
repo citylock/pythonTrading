b770621b7a377660fad34f5bbe32eb9c7d187bb3
citylock <citylock77@gmail.com>
2017-07-03 21:38:30 +0900

    pandas - data input & output

--- dataAnalysis/sec6_ch33_data input and output.py
+++ dataAnalysis/sec6_ch33_data input and output.py
@@ -1,18 +1,23 @@
 import pandas as pd
-
+from sqlalchemy import create_engine
 
 
 # CSV files
 df = pd.read_csv("example.csv")
-
 df.to_csv("My_output.csv", index=False)
 
 
 # Excel files
 pd.read_excel("Excel_Sample.xlsx", Sheetname="Sheet1")
-
-df.to_excel("Excel_sample2.xlsx", sheet_name="NewSheet")
-
+df.to_excel("Excel_sample3.xlsx", sheet_name="NewSheet")
 
 
+# HTML files
+data = pd.read_html('https://www.fdic.gov/bank/individual/failed/banklist.html')
+
+
+# SQL
+engine = create_engine('sqlite:///:memory:')
+df.to_sql('my_table', engine)
+sqldf = pd.read_sql('my_table', con=engine)
 
